fc6b32db872ced3a1d981331746b444c330158d0
Ali Albayrak <l1120910@std.yildiz.edu.tr>
2022-05-15 02:11:33 +0300

    first commit
    main file and respons script + some test scripts

---
+++ respons.py
@@ -0,0 +1,28 @@
+list1 = ()
+list2 = ()
+list3 = ()
+list4 = ()
+list5 = ()
+list6 = ()
+def responses(user_msg):
+    text = str(user_msg).lower()
+
+    if text in list1:
+        pass
+
+    if text in list2:
+        pass
+
+    if text in list3:
+        pass
+
+    if text in list4:
+        pass
+
+    if text in list5:
+        pass
+
+    if text in list6:
+        pass
+    
+    return "i didn't get you!"
---
+++ thread-test.py
@@ -0,0 +1,57 @@
+import threading
+import time
+
+def th(sec: int):
+    time.sleep(sec)
+    print("thread")
+
+t = threading.Thread(target=th,args=(10,))
+t.start()
+print("hello")
+
+
+
+
+
+
+# def threaded(f, daemon=False):
+#     import queue
+
+#     def wrapped_f(q, *args, **kwargs):
+#         '''this function calls the decorated function and puts the 
+#         result in a queue'''
+#         ret = f(*args, **kwargs)
+#         q.put(ret)
+
+#     def wrap(*args, **kwargs):
+#         '''this is the function returned from the decorator. It fires off
+#         wrapped_f in a new thread and returns the thread object with
+#         the result queue attached'''
+
+#         q = queue.Queue()
+
+#         t = threading.Thread(target=wrapped_f, args=(q,)+args, kwargs=kwargs)
+#         t.daemon = daemon
+#         t.start()
+#         t.result_queue = q        
+#         return t
+
+#     return wrap
+
+# @threaded
+# def long_task(x):
+#     import time
+#     x = x + 5
+#     time.sleep(5)
+#     return x
+
+# # does not block, returns Thread object
+# y = long_task(10)
+# # print(y)
+
+# # this blocks, waiting for the result
+# result = y.result_queue.get()
+# print(result)
+
+# for i in range(5):
+#     print(i)
